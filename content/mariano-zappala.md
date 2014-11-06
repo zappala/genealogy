1312e71bf90b014ef3688d06fa83fac1007ce74e
Daniel Zappala <daniel.zappala@gmail.com>
2014-11-06 14:59:53 -0700

    Added content, using a new plugin I wrote.

---
+++ content/mariano-zappala.md
@@ -0,0 +1,23 @@
+Title: Mariano Salvatore Zappala
+Date: 2014-8-24
+Surnames: Zappala, Peters
+People: Mariano Zappala
+Author: Daniel Zappala
+
+Mariano Zappala is my paternal grandfather. He was born in Italy,
+immigrated to America when he was 10, then lived in New York and
+California. This post summarizes what I know about his life at this
+point. I hope some of my relatives can share additional stories,
+photos, and documents.
+
+<!-- PELICAN_END_SUMMARY -->
+
+## Events
+
+## Summary
+
+Mariano was born in [Sersale](http://en.wikipedia.org/wiki/Sersale),
+Italy, which is in the southern party of Italy, near Catanzaro:
+
+![Sersale](/images/places/sersale.png)
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,30 +2,61 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+PATH='content'
+
 AUTHOR = u'Daniel Zappala'
-SITENAME = u'Zappala Genealogy'
-SITEURL = ''
+BIO = u''
+TAGLINE = u''
+SITENAME = u'Finding My Family'
+SITESUB = u'Zappala and Campbell Genealogy'
+SITEURL = 'http://genealogy.zappala.org'
 
-TIMEZONE = 'Europe/Paris'
+# Contact info
+EMAIL = 'dzappala@cedarhills.org'
+FACEBOOK_URL = 'https://www.facebook.com/daniel.zappala'
 
+LICENSE = '<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a>'
+
+TIMEZONE = 'US/Mountain'
 DEFAULT_LANG = u'en'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
+# Tag cloud
+TAG_CLOUD_STEPS = 4
+TAG_CLOUD_MAX_ITEMS = 100
 
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+# How to save pages
+PAGE_URL = '{slug}'
+PAGE_SAVE_AS = '{slug}/index.html'
+ARTICLE_URL = '{slug}/'
+ARTICLE_SAVE_AS = '{slug}/index.html'
+SURNAME_URL = '{slug}'
+SURNAME_SAVE_AS = '/surnames/{slug}.html'
+PERSON_URL = '{slug}'
+PERSON_SAVE_AS = '/people/{slug}.html'
+INDEX_SAVE_AS = 'index.html'
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# Templates
+DIRECT_TEMPLATES = ('index', 'tags', 'archives', 'surnames', 'people')
 
+
+# Feeds
+FEED_DOMAIN = SITEURL
+TAG_FEED_ATOM = 'feeds/tag-%s.atom.xml'
+
+# Misc
+STATIC_PATHS = ['images','docs']
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# Theme
+USER_LOGO_URL = "/images/home/zappala-kids-easter-1973.jpg"
+THEME = "themes/pelican-etna"
+
+# Plugins
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['assets','neighbors', 'summary', 'genealogy']
+
+# Comments
+# DISQUS_SITENAME = 'zappalagenealogy'
+
+# For development only
+RELATIVE_URLS = True
